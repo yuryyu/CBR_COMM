e8a32fdc84a00610b61e4b2c41b7f1236d039d9d
Eli Friedman <friedm3@cooper.edu>
2019-01-27 13:39:17 +0200

    add main file

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import argparse
+from .graph import Graph
+from .features.feature_list import featurizer_list, featurize
+
+
+def main():
+    parseargs = argparse.ArgumentParser()
+    parseargs.add_argument("filename", type=str, help="add an input file to read a graph")
+    parseargs.add_argument("-o", "--output", type=str, default="output.csv", help="add an output file name")
+    args = parseargs.parse_args()
+
+    graph = Graph.load_from_csv(args.filename)
+    features = featurize(featurizer_list, graph)
+    names = [feat.name for feat in features]
+    vals = [feat.value for feat in features]
+    with open(args.output, 'w') as f:
+        f.write(",".join(names)+"\n")
+        f.write(",".join(["%.3f" % val for val in vals]) + "\n")
+
+
+if __name__ == '__main__':
+    main()
