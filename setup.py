a30b2530adddbc72c4ec19419cf5d8cc43f82be9
Eli Friedman <friedm3@cooper.edu>
2019-01-27 16:27:36 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ setup(
     name='intelici-cyber',
     version='0.1',
     description='Repository containing all the Cyber stuff',
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='Intelici',  # Optional
+    author='Intelici',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['networkx', 'igraph'],
-    extras_require={  # Optional
+    install_requires=["numpy", 'networkx', 'python-igraph'],
+    extras_require={
         'test': ['pytest'],
     },
 )
